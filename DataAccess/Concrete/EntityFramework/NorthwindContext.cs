dd46065f9078b0ec77073f6dcf50ac8c47700215
Aras Ateşer <43793683+arasateser@users.noreply.github.com>
2025-05-02 20:48:37 +0300

    abstraction and interfaces

---
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    //context : db tablolari ile proje classlarini baglamak
+    public class NorthwindContext
+    {
+
+    }
+}
